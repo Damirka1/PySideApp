d4dfedf2f9ae050da61957c996bf7771c16d311c
Damirka1 <hamitov.damir3101@icloud.com>
2022-10-12 22:11:50 +0600

    Added menu with buttons

--- Application.py
+++ Application.py
@@ -1,42 +1,70 @@
+from mimetypes import init
 import random
 import sys
 
-from PySide6.QtCore import Qt, Slot
-from PySide6.QtWidgets import (QApplication, QLabel, QPushButton,
-                               QVBoxLayout, QWidget)
-from __feature__ import snake_case, true_property
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt
+# from PySide6.QtGui import QAction
+# from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QToolBar)
 
 
-class Application(QWidget):
+
+
+class Application(QtWidgets.QMainWindow):
     def __init__(self):
-        QWidget.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
 
-        self.hello = [
-            "Hallo Welt",
-            "Hello world",
-            "Hei maailma",
-            "Hola Mundo",
-            "Привет мир!",
-        ]
+        self.setWindowTitle("Lab Solution application")
 
-        self.button = QPushButton("Click me!")
-        self.message = QLabel("Hello World")
-        self.message.alignment = Qt.AlignCenter
+        self.initMenu()
 
-        self.layout = QVBoxLayout(self)
-        self.layout.add_widget(self.message)
-        self.layout.add_widget(self.button)
+    def initMenu(self) -> None:
+        menu = self.menuBar()
+        file_menu = menu.addMenu("Файл")
 
-        # Connecting the signal
-        self.button.clicked.connect(self.magic)
+        create = QtGui.QAction("Создать", self)
+        create.setStatusTip("Основные операции")
+        create.triggered.connect(self.onCreateClicked)
 
-    @Slot()
-    def magic(self):
-        self.message.text = random.choice(self.hello)
+        save = QtGui.QAction("Сохранить", self)
+        save.setStatusTip("Основные операции")
+        save.triggered.connect(self.onSaveClicked)
+
+        clear = QtGui.QAction("Очистить", self)
+        clear.setStatusTip("Основные операции")
+        clear.triggered.connect(self.onClearClicked)
+
+        exit = QtGui.QAction("Выход", self)
+        exit.setStatusTip("Основные операции")
+        exit.triggered.connect(self.onExitClicked)
+
+        file_menu.addAction(create)
+        file_menu.addAction(save)
+        file_menu.addAction(clear)
+        file_menu.addAction(exit)
+
+        file_menu = menu.addMenu("Настройки")
+        file_menu = menu.addMenu("Расчет")
+        file_menu = menu.addMenu("Справка")
+
+
+
+    def onCreateClicked(self, s):
+        print("click", s)
+
+    def onSaveClicked(self, s):
+        print("save", s)
+
+    def onClearClicked(self, s):
+        print("clear", s)
+
+    def onExitClicked(self, s):
+        print("exit", s)
+
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
 
     application = Application()
@@ -44,3 +72,5 @@ if __name__ == "__main__":
     application.show()
 
     sys.exit(app.exec())
+
+# Сделано с душой и любовью за полчаса)
